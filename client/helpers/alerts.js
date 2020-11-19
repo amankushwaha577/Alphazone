f9e2b1d9f8e884e03239ae9c7deaff913d71e736
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-19 22:59:18 +0530

    update

--- client/helpers/alerts.js
+++
@@ -1,2 +0,0 @@
-export const showSuccessMessage = success => <div className="alert alert-success">{success}</div>;
-export const showErrorMessage = error => <div className="alert alert-danger">{error}</div>;
