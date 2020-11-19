13ec4ca7f3be512d64d02a52c8cee53882b7c4b0
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-19 16:01:44 +0530

    category updated

--- client/config.js
+++
@@ -1,8 +0,0 @@
-import getConfig from 'next/config';
-const { publicRuntimeConfig } = getConfig();
-
-export const API = publicRuntimeConfig.API;
-export const APP_NAME = publicRuntimeConfig.APP_NAME;
-export const DOMAIN = publicRuntimeConfig.DOMAIN;
-export const PRODUCTION = publicRuntimeConfig.PRODUCTION;
-export const FB_APP_ID = publicRuntimeConfig.FB_APP_ID;
