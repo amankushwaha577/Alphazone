54f7cb4f2cb29814de0e5e9801303021d72a91e4
Aman Kushwaha <53748350+amankushwaha577@users.noreply.github.com>
2020-11-19 23:56:16 +0530

    Updated

--- client/pages/login.js
+++ client/pages/login.js
@@ -9,8 +9,8 @@ import { authenticate, isAuth } from '../helpers/auth';
 
 const Login = () => {
     const [state, setState] = useState({
-        email: 'reactnodeaws@gmail.com',
-        password: 'rrrrrr',
+        email: 'reactnodeaws57@gmail.com',
+        password: '123456',
         error: '',
         success: '',
         buttonText: 'Login'
--- client/pages/register.js
+++ client/pages/register.js
@@ -8,9 +8,9 @@ import { isAuth } from '../helpers/auth';
 
 const Register = () => {
     const [state, setState] = useState({
-        name: 'Ryan',
-        email: 'ryan@gmail.com',
-        password: 'rrrrrr',
+        name: 'Aman Kushwaha',
+        email: 'reactnodeaws57@gmail.com',
+        password: '123456',
         error: '',
         success: '',
         buttonText: 'Register',
